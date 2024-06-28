836f8ff4b64071379793aafc8b02f0d2a6fb1a13
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-06-28 14:38:55 -0600

    feat: routing Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
+import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomePageComponent
+  },
+  {
+    path: 'about',
+    component: AboutPageComponent
+  },
+  {
+    path: '**',
+    component: HomePageComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
