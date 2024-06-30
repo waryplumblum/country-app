2cd171517815d04b0065fbd8b5c78125da6ce57b
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-06-29 23:24:09 -0600

    feat: new country table component

---
+++ src/app/countries/components/country-table/country-table.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Country } from '../../interfaces/country';
+
+@Component({
+  selector: 'countries-country-table',
+  templateUrl: './country-table.component.html',
+  styleUrl: './country-table.component.css'
+})
+export class CountryTableComponent {
+
+  @Input()
+  public countries:Country[]=[];
+}
