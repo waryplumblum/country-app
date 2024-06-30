b8cf72428b9690bef9cb12d9f1c1445059459af7
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-06-30 14:58:20 -0600

    switchMap

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.services';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
@@ -11,17 +12,22 @@ export class CountryPageComponent implements OnInit{
 
   constructor(
     private activatedRoute:ActivatedRoute,
-    private countriesService:CountriesService
+    private countriesService:CountriesService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
     this.activatedRoute.params
-      .subscribe( ({ id }) => {
-        this.countriesService.searchCountryByAlphaCode( id )
-          .subscribe( country => {
-            console.log({ country })
-          });
-      });
+    .pipe(
+      switchMap( ({ id }) => this.countriesService.searchCountryByAlphaCode( id )),
+    )
+    .subscribe( country => {
+      if( !country ){
+        return this.router.navigateByUrl('');
+      }
+      console.log('Tenemos un país.');
+      return;
+    });
   }
 
 
