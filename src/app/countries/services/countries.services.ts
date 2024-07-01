52d7e08c32a12864bfa2ffdf5498b755959a55c3
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-01 14:14:07 -0600

    feat: localStorage

--- src/app/countries/services/countries.services.ts
+++ src/app/countries/services/countries.services.ts
@@ -16,7 +16,16 @@ export class CountriesService {
     byRegion:     {  region: '', countries: []  },
   }
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { this.loadFromLocalStorage(); }
+
+  private saveToLocalStorage() {
+    localStorage.setItem( 'cacheStore', JSON.stringify( this.cacheStore ));
+  }
+
+  private loadFromLocalStorage() {
+    if( !localStorage.getItem('cacheStore') ) return;
+    this.cacheStore = JSON.parse( localStorage.getItem('cacheStore')! );
+  }
 
   private getCountriesRequest( url:string ):Observable<Country[]>{
     return this.http.get<Country[]>( url )
@@ -39,7 +48,8 @@ export class CountriesService {
     const url = `${ this.apiUrl }/capital/${ term }`;
     return this.getCountriesRequest( url )
     .pipe(
-      tap( countries => this.cacheStore.byCapital = { term,countries })
+      tap( countries => this.cacheStore.byCapital = { term,countries }),
+      tap( () => this.saveToLocalStorage() ),
     );
   }
 
@@ -47,7 +57,8 @@ export class CountriesService {
     const url = `${ this.apiUrl }/name/${ term }`;
     return this.getCountriesRequest( url )
     .pipe(
-      tap( countries => this.cacheStore.byCountries = { term,countries })
+      tap( countries => this.cacheStore.byCountries = { term,countries }),
+      tap( () => this.saveToLocalStorage() ),
     );
   }
 
@@ -55,7 +66,8 @@ export class CountriesService {
     const url = `${ this.apiUrl }/region/${ region }`;
     return this.getCountriesRequest( url )
     .pipe(
-      tap( countries => this.cacheStore.byRegion = { region,countries })
+      tap( countries => this.cacheStore.byRegion = { region,countries }),
+      tap( () => this.saveToLocalStorage() ),
     );
   }
 }
