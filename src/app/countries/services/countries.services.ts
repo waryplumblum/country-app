cd38803fd34640f944384d0b48f44fba9f0d1ee6
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-01 10:35:59 -0600

    feat: debounce

--- src/app/countries/services/countries.services.ts
+++ src/app/countries/services/countries.services.ts
@@ -14,7 +14,7 @@ export class CountriesService {
     return this.http.get<Country[]>( url )
       .pipe(
         catchError( error => of([])),
-        delay( 2000 )
+        // delay( 2000 )
       );
   }
 
--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,11 +1,14 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Subject, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styles: ``
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit {
+
+  private debouncer: Subject<string> = new Subject<string>();
 
   @Input()
   public placeholder: string = '';
@@ -13,7 +16,25 @@ export class SearchBoxComponent {
   @Output()
   public onValue = new EventEmitter<string>();
 
+  @Output()
+  public onDebounce = new EventEmitter<string>();
+
+  ngOnInit(): void {
+    this.debouncer
+    .pipe(
+      debounceTime(500)
+    )
+    .subscribe( value => {
+      this.onDebounce.emit( value );
+    })
+  }
+
   emitValue( value:string ):void {
     this.onValue.emit( value );
   }
+
+  onKeyPress( searchTerm:string) {
+    this.debouncer.next( searchTerm );
+  }
+
 }
